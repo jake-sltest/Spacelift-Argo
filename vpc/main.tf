a34f6016ba4201acc0ddc90a25b61297dd64c41c
Jake <jacobj@spacelift.io>
2024-04-04 21:35:32 -0700

    updated cidr

--- vpc/main.tf
+++ vpc/main.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "argo_subnet_one" {
 
 resource "aws_subnet" "argo_subnet_two" {
     vpc_id = aws_vpc.argo_vpc.id
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "true"
     availability_zone = "us-east-1b"
     tags = {
