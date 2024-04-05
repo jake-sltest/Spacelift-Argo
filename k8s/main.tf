cec374ebf8da6374a5332a3ade9a09f749bb7a89
Jake <jacobj@spacelift.io>
2024-04-04 21:38:10 -0700

    added new subnet

--- k8s/main.tf
+++ k8s/main.tf
@@ -17,7 +17,7 @@ resource "aws_eks_node_group" "node_group_one" {
   cluster_name    = aws_eks_cluster.my_cluster.name
   node_group_name = "node-group-1"
   node_role_arn   = "arn:aws:iam::993741443707:role/spacelift-main"
-  subnet_ids = ["subnet-066b3055952be9d0c" ]
+  subnet_ids = ["subnet-066b3055952be9d0c, subnet-0ff1423d91026dc01" ]
   
   scaling_config {
     desired_size = 1
