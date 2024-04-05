821a642d9f05f0fa81b8989723b4a896175e5d55
Jake <jacobj@spacelift.io>
2024-04-04 20:19:29 -0700

    add base infra resources

--- vpc/main.tf
+++ vpc/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_vpc" "sd_vpc" {
+resource "aws_vpc" "argo_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
@@ -20,7 +20,7 @@ resource "aws_vpc" "sd_vpc" {
   }
 }
 
-resource "aws_subnet" "sd_subnet" {
+resource "aws_subnet" "argo_subnet" {
     vpc_id = aws_vpc.sd_vpc.id
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
