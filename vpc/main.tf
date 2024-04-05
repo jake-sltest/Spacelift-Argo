7af8c8037791183ef9f3cb2af9947fa6e1700641
Jake <jacobj@spacelift.io>
2024-04-04 20:20:49 -0700

    fixed copy paste issue

--- vpc/main.tf
+++ vpc/main.tf
@@ -21,7 +21,7 @@ resource "aws_vpc" "argo_vpc" {
 }
 
 resource "aws_subnet" "argo_subnet" {
-    vpc_id = aws_vpc.sd_vpc.id
+    vpc_id = aws_vpc.argo_vpc.id
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
 
