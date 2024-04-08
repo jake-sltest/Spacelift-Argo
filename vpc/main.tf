1ece024334670500d4d6bdc0f7bed9da76c96a34
Jake <jacobj@spacelift.io>
2024-04-08 10:39:53 -0700

    fixed reference to subnet

--- vpc/main.tf
+++ vpc/main.tf
@@ -44,7 +44,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public-us-east-1a.id
+  subnet_id     = aws_subnet.argo_subnet_one.id
 
   tags = {
     Name = "nat"
